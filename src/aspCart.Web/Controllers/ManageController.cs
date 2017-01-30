4f528c5f61384cad6bc3f1b2a60f3963a5e552c6
marklaygo <marktlaygo@gmail.com>
2017-01-30 11:07:49 +0800

    Add project files.

---
+++ src/aspCart.Web/Controllers/ManageController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using aspCart.Web.Models;
+using aspCart.Web.Models.ManageViewModels;
+
+namespace aspCart.Web.Controllers
+{
+    [Authorize]
+    public class ManageController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public ManageController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ILoggerFactory loggerFactory)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _logger = loggerFactory.CreateLogger<ManageController>();
+        }
+
+        //
+        // GET: /Manage/Index
+        [HttpGet]
+        public async Task<IActionResult> Index(ManageMessageId? message = null)
+        {
+            ViewData["StatusMessage"] =
+                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
+                : message == ManageMessageId.Error ? "An error has occurred."
+                : "";
+
+            var user = await GetCurrentUserAsync();
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var model = new IndexViewModel
+            {
+                HasPassword = await _userManager.HasPasswordAsync(user),
+                Logins = await _userManager.GetLoginsAsync(user),
+                BrowserRemembered = await _signInManager.IsTwoFactorClientRememberedAsync(user)
+            };
+            return View(model);
+        }
+        
+        //
+        // GET: /Manage/ChangePassword
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Manage/ChangePassword
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await GetCurrentUserAsync();
+            if (user != null)
+            {
+                var result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation(3, "User changed their password successfully.");
+                    return RedirectToAction(nameof(Index), new { Message = ManageMessageId.ChangePasswordSuccess });
+                }
+                AddErrors(result);
+                return View(model);
+            }
+            return RedirectToAction(nameof(Index), new { Message = ManageMessageId.Error });
+        }
+
+        #region Helpers
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
+        }
+
+        public enum ManageMessageId
+        {
+            ChangePasswordSuccess,
+            Error
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync()
+        {
+            return _userManager.GetUserAsync(HttpContext.User);
+        }
+
+        #endregion
+    }
+}
---
+++ src/aspCart.Web/gulpfile.js
@@ -0,0 +1,35 @@
+﻿"use strict";
+
+var gulp = require("gulp");
+
+var paths = {
+    lib: './wwwroot/lib/',
+    node: './node_modules/'
+}
+
+gulp.task("copy-dependencies", function () {
+    // bootstrap
+    gulp.src([
+        paths.node + 'bootstrap/dist/**/*',
+        '!' + paths.node + 'bootstrap/dist/**/*.map'
+    ])
+        .pipe(gulp.dest(paths.lib + 'bootstrap/dist'));
+
+    // jquery
+    gulp.src([
+        paths.node + 'jquery/dist/**/*',
+        '!' + paths.node + 'jquery/dist/**/*.map'
+    ])
+        .pipe(gulp.dest(paths.lib + 'jquery/dist'));
+
+    // jquery validation
+    gulp.src([
+        paths.node + 'jquery-validation/dist/jquery.validate.js',
+        paths.node + 'jquery-validation/dist/jquery.validate.min.js'
+    ])
+        .pipe(gulp.dest(paths.lib + 'jquery-validation/dist'));
+
+    // jquery validation unobtrusive
+    gulp.src(paths.node + 'jquery-validation-unobtrusive/jquery.validate.unobtrusive.js')
+        .pipe(gulp.dest(paths.lib + 'jquery-validation-unobtrusive'));
+});
